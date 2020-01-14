bbe96e5cec649f5995db9cdea1960db351e24479
j <j@HOME.LAN>
2020-01-14 01:10:13 -0500

    Added all the general attributes and properties immediately accessible on the craft instance Some are commented out due to types not being handled (output in dev console) - mainly bool, int32, and quad

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import socket, struct
+from os import system, name
+from time import sleep
 
 TypeFormats = [
     "f",
@@ -8,8 +10,74 @@ TypeFormats = [
     "iii"]
 
 FieldNames = [
-    "Velocity",
-    "Altitude"]
+       "Velocity",
+       "Altitude",
+       "AltitudeAgl",
+       #"CanWarp",
+       "Brake",
+       "Pitch",
+       "Roll",
+       "Slider1",
+       "Slider2",
+       #"TargetDirection",
+       #"TargetHeading",
+       "Throttle",
+       "TranslateForward",
+       "TranslateRight",
+       "TranslateUp",
+       #"TranslationModeEnabled",
+       "Yaw",
+       "CraftMass",
+       #"CraftPartCount",
+       #"CurrentStage",
+       #"NumStages",
+       "AirDensity",
+       "AirPressure",
+       "AtmosphereHeight",
+       "SampleAltitude",
+       "SpeedOfSound",
+       "Temperature",
+       "eulerAngles",
+       "forward",
+       "localEulerAngles",
+       "localPosition",
+       #"localRotation",
+       "localScale",
+       "position",
+       "right",
+       #"rotation",
+       "up",
+       "CurrentEngineThrust",
+       "GravityMagnitude",
+       "MachNumber",
+       "MaxActiveEngineThrust",
+       "RemainingBattery",
+       "RemainingFuelInStage",
+       "RemainingMonopropellant",
+       "SolarRadiationDirection",
+       "SolarRadiationFrameDirection",
+       "SolarRadiationIntensity",
+       "SurfaceVelocity",
+       "SurfaceVelocityMagnitude",
+       "VelocityMagnitude",
+       "WeightedThrottleResponse",
+       "WeightedThrottleResponseTime",
+       "FramePosition",
+       "GravityForce",
+       "GravityNormal",
+       "AirEfficiency",
+       "AvailableAir",
+       "ReEntryIntensity",
+       #"Heading",
+       #"InContactWithPlanet",
+       #"IsDestroyed",
+       "Position",
+       "SolarPosition",
+       "SolarVelocity",
+       "SphereOfInfluence",
+       #"SurfacePosition",
+       #"SurfaceRotation"
+       ]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -20,8 +88,6 @@ def process(data):
     fmt = TypeFormats[tp]
     val = struct.unpack(fmt, data[8:8 + struct.calcsize(fmt)])
     print(FieldNames[nm], ": ", val, sep="")
-    
-    
 
 while 1:
         d, a = s.recvfrom(256)
